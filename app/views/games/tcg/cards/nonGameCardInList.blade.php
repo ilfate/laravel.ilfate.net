dac26fe002dd63e90dcdde3c25ec9996ccbe15a6
ilthefate <ilthefate@gmail.com>
2014-12-17 11:21:30 +0100

    Tcg. test commit 2

--- app/views/games/tcg/cards/nonGameCardInList.blade.php
+++ app/views/games/tcg/cards/nonGameCardInList.blade.php
@@ -1,5 +1,7 @@
 
-<div class="non-game-card-in-list id_{{$card['id']}}" data-id="{{$card['id']}}">
-    <div class="image" style="background-image:url('/images/game/tcg/{{$card['image']}}')"></div>
-	
+<div class="list-card-container">
+	<div class="non-game-card-in-list id_{{$card['id']}}" data-id="{{$card['id']}}">
+	    <div class="image" style="background-image:url('/images/game/tcg/{{$card['image']}}')"></div>
+		
+	</div>
 </div>
